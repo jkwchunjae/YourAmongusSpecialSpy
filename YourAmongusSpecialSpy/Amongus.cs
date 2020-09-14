8342fc28d2d1e7052bdd312b89b2626e776e4b84
jkw <jkwchunjae@daum.net>
2020-09-14 20:27:07 +0900

    Amongus helper

---
+++ YourAmongusSpecialSpy/Amongus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourAmongusSpecialSpy
+{
+    public static class Amongus
+    {
+        private static Process GetProcess()
+            => Process.GetProcessesByName("Among Us").FirstOrDefault();
+
+        public static Bitmap GetImage()
+            => GetProcess()?.CaptureWindow();
+
+    }
+}
