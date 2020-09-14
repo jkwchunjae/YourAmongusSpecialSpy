4f9946cb0003b28fb7f5423c18c51f6db4ebb428
jkw <jkwchunjae@daum.net>
2020-09-14 20:26:56 +0900

    Process extensions

---
+++ YourAmongusSpecialSpy/ProcessExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourAmongusSpecialSpy
+{
+    public class User32
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowDC(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowRect(IntPtr hWnd, ref RECT rect);
+        [DllImport("user32.dll")]
+        public static extern bool PrintWindow(IntPtr hwnd, IntPtr hdcBlt, uint nFlags);
+    }
+
+
+    public static class ProcessExtensions
+    {
+        public static Bitmap CaptureWindow(this Process process)
+        {
+            var rect = new User32.RECT();
+            User32.GetWindowRect(process.MainWindowHandle, ref rect);
+
+            int width = rect.right - rect.left;
+            int height = rect.bottom - rect.top;
+
+            var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            Graphics graphics = Graphics.FromImage(bmp);
+            graphics.CopyFromScreen(rect.left, rect.top, 0, 0, new Size(width, height), CopyPixelOperation.SourceCopy);
+            return bmp;
+        }
+    }
+}
