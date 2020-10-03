8b58773ddaf6221cd5e779132811b6c2fb9fd2f9
jkw <jkwchunjae@daum.net>
2020-10-03 21:54:08 +0900

    Mouse controller

---
+++ YourAmongusSpecialSpy/MouseController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourAmongusSpecialSpy
+{
+    public static class MouseController
+    {
+        public static void Move(int x, int y)
+        {
+            var p = Amongus.GetProcess();
+            var hWnd = p.MainWindowHandle;
+            var rect = new User32.RECT();
+            User32.GetWindowRect(hWnd, ref rect);
+
+            var windowX = rect.left;
+            var windowY = rect.top;
+
+            SetCursorPos(windowX + x, windowY + y);
+        }
+
+        public static void Move(Point point)
+        {
+            Move(point.X, point.Y);
+        }
+
+        public static void Click(int x, int y)
+        {
+            Move(x, y);
+
+            mouse_event((uint)(MouseEventFlags.LEFTDOWN), 0, 0, 0, 0);
+            mouse_event((uint)(MouseEventFlags.LEFTUP), 0, 0, 0, 0);
+        }
+
+        public static void LeftDown(Point point)
+        {
+            Move(point);
+
+            mouse_event((uint)(MouseEventFlags.LEFTDOWN), 0, 0, 0, 0);
+        }
+
+        public static void LeftUp(Point point)
+        {
+            Move(point);
+
+            mouse_event((uint)(MouseEventFlags.LEFTUP), 0, 0, 0, 0);
+        }
+
+        private enum MouseEventFlags
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010
+        }
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+
+        [DllImport("User32.Dll")]
+        private static extern long SetCursorPos(int x, int y);
+
+    }
+}
